aef2f9e576664e5ee599c6a17ced6c9b757c8df0
MatthewScott <mattwillscott95@gmail.com>
2022-09-07 17:43:32 -0400

    created CreateWatchlist component and built out

---
+++ src/components/CreateWatchlist.jsx
@@ -0,0 +1,66 @@
+const CreateWatchlist = () => {
+  const initialState = {
+    name: '',
+    stocks: []
+  }
+  const [formState, setFormState] = useState(initialState)
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.id]: event.target.value })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    console.log(formState)
+
+    let res = await axios.post('http://localhost:8000/watchlists', formState)
+    setFormState(initialState)
+  }
+  return (
+    <div className="create-watchlist">
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="watchlistInputs">
+          {/* <label htmlFor="watchlist">Watchlist Name:</label>
+          <select id="manga" onChange={handleChange} value={formState.manga}>
+            <option value="blank">---------</option>
+
+            {props.mangas.map((manga, index) => (
+              <option value={manga.title} key={index}>
+                {manga.title}
+              </option>
+            ))}
+          </select> */}
+
+          <label htmlFor="name">Watchlist Name:</label>
+          <input
+            type="text"
+            id="name"
+            onChange={handleChange}
+            value={formState.name}
+          />
+
+          <label htmlFor="stock">Insert Ticker Symbol:</label>
+          <input
+            type="text"
+            id="ticker"
+            onChange={handleChange}
+            value={formState.stocks}
+          />
+        </div>
+        {/* <div className="reviewText">
+          <label htmlFor="text">Leave your review here:</label>
+          <textarea
+            id="text"
+            cols="30"
+            rows="10"
+            onChange={handleChange}
+            value={formState.text}
+          ></textarea>
+        </div> */}
+        <button type="submit">Create Watchlist!</button>
+      </form>
+    </div>
+  )
+}
+
+export default CreateWatchlist
