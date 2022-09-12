e894417057bdb0d6cac658629576d313204c5536
MatthewScott <mattwillscott95@gmail.com>
2022-09-12 13:48:05 -0400

    10day avg volume

--- src/components/BasicFinancials.jsx
+++ src/components/BasicFinancials.jsx
@@ -3,23 +3,23 @@ import React from 'react'
 const BasicFinancials = ({ financials }) => {
   console.log(financials)
   //   let basics = financials.metric
-  return (
-    <div className="homePage">
-      <h1>Financials</h1>
-      <div>
-        <p>52 Week Range:</p>
-        <p>
-          ${financials['52WeekLow']} - ${financials['52WeekHigh']}
-        </p>
+  if (financials) {
+    return (
+      <div className="homePage">
+        <h1>Financials</h1>
+        <div>
+          <p>52 Week Range:</p>
+          <p>
+            ${financials['52WeekLow']} - ${financials['52WeekHigh']}
+          </p>
+        </div>
+        <div>
+          <p>10-Day Avg. Volume:</p>
+          <p>{financials['10DayAverageTradingVolume']} million</p>
+        </div>
       </div>
-      <div>
-        <p>52 Week Range:{financials['52WeekHigh']}</p>
-        <p>
-          ${financials['52WeekLow']} - ${financials['52WeekHigh']}
-        </p>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default BasicFinancials
