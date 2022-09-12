972712ea7f9b853deb7963a10873fa8881933540
MatthewScott <mattwillscott95@gmail.com>
2022-09-11 23:23:40 -0400

    testing the candle stick data

--- testing.js
+++ testing.js
@@ -4,22 +4,22 @@ const api_key = finnhub.ApiClient.instance.authentications['api_key']
 api_key.apiKey = 'cc8atrqad3iciiq4952g'
 const finnhubClient = new finnhub.DefaultApi()
 
-finnhubClient.companyNews(
-  'AAPL',
-  '2020-01-01',
-  '2022-09-01',
-  (error, data, response) => {
-    if (error) {
-      console.error(error)
-    } else {
-      console.log(data)
-    }
-  }
-)
-finnhubClient.quote('AAPL', (error, data, response) => {
-  console.log(data)
-})
-
+// finnhubClient.companyNews(
+//   'AAPL',
+//   '2020-01-01',
+//   '2022-09-01',
+//   (error, data, response) => {
+//     if (error) {
+//       console.error(error)
+//     } else {
+//       console.log(data)
+//     }
+//   }
+// )
+// finnhubClient.quote('AAPL', (error, data, response) => {
+//   console.log(data)
+// })
+let currentTimestamp = Date.now()
 finnhubClient.stockCandles(
   'AAPL',
   'D',
@@ -29,3 +29,4 @@ finnhubClient.stockCandles(
     console.log(data)
   }
 )
+console.log(currentTimestamp - 864000)
