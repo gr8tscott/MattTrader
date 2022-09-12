39c301bf2da7ce21486ef8a541cfc8f30e32709e
MatthewScott <mattwillscott95@gmail.com>
2022-09-12 14:03:40 -0400

    the financials have boxes, not great yet

--- src/components/BasicFinancials.jsx
+++ src/components/BasicFinancials.jsx
@@ -6,17 +6,34 @@ const BasicFinancials = ({ financials }) => {
   if (financials) {
     return (
       <div className="homePage">
-        <h1>Financials</h1>
-        <div>
-          <p>52 Week Range:</p>
-          <p>
-            ${financials['52WeekLow']} - ${financials['52WeekHigh']}
-          </p>
+        <div className="financialBoxes">
+          <h1>Financials</h1>
+          <div>
+            <p>Beta:</p>
+            <p>{financials['beta']}</p>
+          </div>
+          <div>
+            <p>52 Week Range:</p>
+            <p>
+              ${financials['52WeekLow']} - ${financials['52WeekHigh']}
+            </p>
+          </div>
+          <div>
+            <p>10-Day Avg. Volume:</p>
+            <p>{financials['10DayAverageTradingVolume']} million</p>
+          </div>
+          <div>
+            <p>90-Day Avg. Volume:</p>
+            <p>{financials['3MonthAverageTradingVolume']} million</p>
+          </div>
         </div>
-        <div>
-          <p>10-Day Avg. Volume:</p>
-          <p>{financials['10DayAverageTradingVolume']} million</p>
-        </div>
+        {/* <div>
+          <h3>Dividends</h3>
+          <div>
+            <p>90-Day Avg. Volume:</p>
+            <p>{financials['3MonthAverageTradingVolume']} million</p>
+          </div>
+        </div> */}
       </div>
     )
   }
--- testing.js
+++ testing.js
@@ -30,3 +30,7 @@ finnhubClient.stockCandles(
   }
 )
 console.log(currentTimestamp - 864000)
+
+// let currentTimestamp = Date.now()
+// console.log(currentTimestamp); // get current timestamp
+// let date = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }).format(currentTimestamp)
